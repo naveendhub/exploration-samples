0a78f55872e428647eb1da7756c99ba5883ed6d5
Naveen D <naveen.d@philips.com>
2023-06-05 11:50:02 +0530

    Utility

---
+++ Utilities/launchProcesses.ps1
@@ -0,0 +1,22 @@
+﻿param
+(
+    [int]$numberOfProcesses = 1,
+    [string]$exeLocation = "C:\Workspace\Source\exploration-samples\Dotnet-samples\exploration-samples\exploration-samples\bin\Debug\exploration-samples.exe"
+
+)
+
+
+try {
+    # Set strict mode, to prevent nasty debugging sessions
+    Set-StrictMode -Version 5.1
+    for ($i=1; $i -le $numberOfProcesses; $i++)
+    {
+        Start-Process $exeLocation
+    }
+
+}
+catch {
+    Write-Host "Launch Failed: $($PSItem.ToString())" -ForegroundColor Red
+} finally {
+    Set-StrictMode -Off
+}
